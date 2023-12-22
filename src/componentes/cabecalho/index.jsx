5926c1180786e419eec92e0e625f6a4dad610200
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2023-12-22 20:43:07 -0300

    Cleanup Primario

---
+++ src/componentes/cabecalho/index.jsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css'
+
+export default function Cabecalho() {
+    return (
+        <header>
+            <h2>Sistema Agendamento</h2>
+        </header>
+    )
+}
