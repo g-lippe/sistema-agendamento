333b0abf1c109018f075d2e12b58d67877a4fa57
Caio Camizão <caio_quimica2013@hotmail.com>
2023-12-24 15:19:36 -0300

    chore: "Creation of the 'menu' folder with index and CSS files

--- src/componentes/cabecalho/index.jsx
+++ src/componentes/cabecalho/index.jsx
@@ -1,4 +1,4 @@
-import styles from './index.module.css'
+import styles from './cabecalho.module.css'
 
 export default function Cabecalho() {
     return (
---
+++ src/componentes/menu/index.jsx
@@ -0,0 +1,4 @@
+import styles from './menu.module.css'
+
+export default function Menu() {
+}
