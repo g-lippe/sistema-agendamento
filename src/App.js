8c9f895df142cdeab9cffc509dafa2b8b932f372
Caio Camizão <caio_quimica2013@hotmail.com>
2023-12-26 23:43:01 -0300

    chore: I created a folder called "sidebar" where there is a menu icon, and inside it, I will place items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Cabecalho from './componentes/cabecalho';
+import Menu from './componentes/menu/sidebar-hamburguermenu';
 
 function App() {
   return (
     <div className="App">
+      <div className='content'>
+        <Menu/>
+      </div>
       <header className="App-header">
-
         <Cabecalho/>
-
       </header>
     </div>
   );
