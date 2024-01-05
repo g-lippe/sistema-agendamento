798dffb44895c5b293371c7faa543c429017f11c
Caio Camizão <caio_quimica2013@hotmail.com>
2024-01-04 23:20:54 -0300

    feat: Installation of the react-router-dom component to start creating the login page and fixing the footer's front-end

--- src/componentes/rodape/index.jsx
+++ src/componentes/rodape/index.jsx
@@ -3,11 +3,13 @@ import styles from "./rodape.module.scss";
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
+import { BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Rodape = () => {
   return (
     <footer>
-      <p>Nossas redes sociais</p>
+      <p>Nossas redes sociais abaixo:</p>
       <FaFacebook className={styles.facebookLogo} />
       <FaInstagram className={styles.instagramLogo} />
       <FaWhatsapp className={styles.whatsappLogo} />
