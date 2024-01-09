f9f77cd8cc86763a4b7452cb5478e41fabdd2133
Caio Camizão <caio_quimica2013@hotmail.com>
2024-01-08 22:08:29 -0300

    feat: init creating of routes for user login

---
+++ src/componentes/routes/loginUser.jsx
@@ -0,0 +1,14 @@
+import { React, useState } from "react";
+import styles from "./loginUser.module.scss";
+import Sidebar from "componentes/menu/Sidebar";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, usePassword] = useState("");
+};
+
+return (
+
+)
+
+export default Login;
