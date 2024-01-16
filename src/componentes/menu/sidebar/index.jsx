c0d95e6c472708d0244df4e28b020198667165be
Caio Camizão <caio_quimica2013@hotmail.com>
2024-01-15 23:20:51 -0300

    style: include icon closeMenubar and hover_shadow in sideMenubar

--- src/componentes/menu/sidebar/index.jsx
+++ src/componentes/menu/sidebar/index.jsx
@@ -1,6 +1,7 @@
 // Sidebar.jsx
 import React, { useState } from "react";
 import { CiMenuBurger } from "react-icons/ci";
+import { IoCloseSharp } from "react-icons/io5";
 import SidebarItem from "../SidebarItem";
 import styles from "./sideBar.module.scss";
 import search from "./icons8-lupa-24.png";
@@ -13,11 +14,25 @@ const Sidebar = () => {
     setMenuVisible(!menuVisible);
   };
 
+  const closeMenu = () => {
+    setMenuVisible(false);
+  };
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.nameContainer}>
-        <CiMenuBurger className={styles.sidebarIcon} onClick={toggleMenu} />
-        <GiSittingDog className={styles.logoIcon} />
+        {menuVisible ? (
+          <IoCloseSharp
+            className={styles.closeIcon + " hover_shadow"}
+            onClick={closeMenu}
+          />
+        ) : (
+          <CiMenuBurger
+            className={styles.sidebarIcon + " hover_shadow"}
+            onClick={toggleMenu}
+          />
+        )}
+        <GiSittingDog className={styles.logoIcon + " hover_shadow"} />
         <h4>DataPet</h4>
         <div className={styles.sidebar__container}>
           <input
