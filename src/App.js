1a9f45dbafcfe29001a5c579922190cc3a2a7547
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2024-01-19 15:19:53 -0300

    Alterado App para editar Tela de Login
    Apenas temporário até Router estiver completo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomePage from "paginas/homePage";
 import './styles/estilosGlobais.scss';
+import TelaLogin from "paginas/login";
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <TelaLogin />
     </div>
   );
 };
