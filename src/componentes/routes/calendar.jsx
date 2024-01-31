a6d069a953572a745341ad37576821d8515d4cb9
Caio Camizão <caio_quimica2013@hotmail.com>
2024-01-30 22:52:34 -0300

    feat: add library react big calendar in calendar routes and increment link texts in settings

--- src/componentes/routes/calendar.jsx
+++ src/componentes/routes/calendar.jsx
@@ -1,15 +1,39 @@
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import "moment/locale/pt-br";
 import Sidebar from "componentes/menu/Sidebar";
 import Rodape from "componentes/rodape";
 
-const calendar = () => {
+const localizer = momentLocalizer(moment);
+
+const CalendarDate = () => {
+  const events = [
+    {
+      title: "Evento 1",
+      start: new Date(2024, 0, 30, 10, 0),
+      end: new Date(2024, 0, 30, 12, 0),
+    },
+  ];
+
   return (
     <div>
       <Sidebar />
-      <h1>Calendario</h1>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{
+          height: "500px",
+          width: "95%",
+          margin: "auto",
+        }}
+      />
       <Rodape />
     </div>
   );
 };
 
-export default calendar;
+export default CalendarDate;
--- src/componentes/routes/settings.jsx
+++ src/componentes/routes/settings.jsx
@@ -1,12 +1,42 @@
 import Sidebar from "componentes/menu/Sidebar";
 import Rodape from "componentes/rodape";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const settings = () => {
   return (
     <div>
       <Sidebar />
-      <h1>Configuracoes</h1>
+      <Link
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        Editar Perfil
+      </Link>
+      <Link
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        Historico
+      </Link>
+      <Link
+        style={{
+          marginTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        Sair da Conta
+      </Link>
       <Rodape />
     </div>
   );
